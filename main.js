ce63d37dd00cb66de6bfd56787dae0c25ae2bc92
Carlos Moreno <carlosmm2013@gmail.com>
2023-03-08 11:48:05 +0100

    Añadiendo animación de salida

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ let label = document.getElementById("label");
 let checkbox = document.getElementById("checkbox");
 
 label.addEventListener("click", ()=> {
-    label.classList.toggle('bubble-out');
+    /* label.classList.toggle('bubble-out'); */
+    label.classList.add('bubble-out');
+    setTimeout(() => {
+        label.classList.remove("bubble-out");
+    }, 1000);
+    
 })
