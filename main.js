7c3bdc11c8c3dc0b7624082f79d922f022a067b7
Carlos Moreno <carlosmm2013@gmail.com>
2023-03-08 12:22:44 +0100

    Añadiendo estilos

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 let label = document.getElementById("label");
-let checkbox = document.getElementById("checkbox");
+
 
 label.addEventListener("click", ()=> {
-    /* label.classList.toggle('bubble-out'); */
     label.classList.add('bubble-out');
     setTimeout(() => {
         label.classList.remove("bubble-out");
-    }, 1000);
+    }, 500);
     
 })
